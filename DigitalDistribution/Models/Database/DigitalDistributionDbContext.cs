d0205bcd7fc26680d182c0ee548f020fd0fe98be
Miron Catalin Andrei <cata.miron98@gmail.com>
2021-03-26 14:02:58 +0200

    Add project files.

---
+++ DigitalDistribution/Models/Database/DigitalDistributionDbContext.cs
@@ -0,0 +1,23 @@
+﻿using DigitalDistribution.Models.Database.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DigitalDistribution.Models.Database
+{
+    public class DigitalDistributionDbContext : DbContext
+    {
+        public DigitalDistributionDbContext(DbContextOptions<DigitalDistributionDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<UserEntity> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ DigitalDistribution/Services/UserEntityService.cs
@@ -0,0 +1,44 @@
+﻿using DigitalDistribution.Models.Database.Entities;
+using DigitalDistribution.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DigitalDistribution.Services
+{
+    public class UserEntityService
+    {
+        private readonly UserEntityRepository _userEntityReposityory;
+
+        public UserEntityService(UserEntityRepository userEntityRepository)
+        {
+            _userEntityReposityory = userEntityRepository;
+        }
+        public async Task<UserEntity> CreateUser(UserEntity user)
+        {
+            UserEntity result = await _userEntityReposityory.Insert(user);
+            return result;
+        }
+
+        public async Task<UserEntity> GetByUsername(string username)
+        {
+            return await _userEntityReposityory.GetByUsername(username);
+        }
+
+        public async Task<List<UserEntity>> GetAll()
+        {
+            return await _userEntityReposityory.GetAll();
+        }
+
+        public async Task<bool> Delete(string username)
+        {
+            return await _userEntityReposityory.Delete(username);
+        }
+        public async Task<UserEntity> Update(UserEntity user)
+        {
+            return await _userEntityReposityory.Update(user);
+        }
+
+    }
+}
